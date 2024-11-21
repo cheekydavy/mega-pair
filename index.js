9518809e308553caa5973ae8dabc2ad2f8882958
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-11-21 08:05:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`YoutTube: @GlobalTechInfo\n\nGitHub: @GlobalTechInfo\n\nServer running on http://localhost:${PORT}`);
+    console.log(`YoutTube: @rhodvick\n\nGitHub: @cheekydavy\n\nServer running on http://localhost:${PORT}`);
 });
 
 export default app;
